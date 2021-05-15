6e936b1c45882d7325eff9809c3772b1b3d64082
Haryati H <haryati75@gmail.com>
2021-05-15 13:03:38 +0800

    Inital folder, files with leaflet map setup

---
+++ script.js
@@ -0,0 +1,15 @@
+let singapore = [1.29, 103.85]; // Singapore latlng
+let map = L.map('map').setView(singapore, 13); // set the center point
+
+// setup the tile layers
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+   maxZoom: 18,
+   id: 'mapbox/streets-v11',
+   tileSize: 512,
+   zoomOffset: -1,
+   accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw' //demo access token
+}).addTo(map);
+
+
+
